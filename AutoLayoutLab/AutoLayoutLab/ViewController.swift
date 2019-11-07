eee06b7c52f0d0699505a6905d2831a6fdeacb12
maitreebain <maitreebain@gmail.com>
2019-11-07 15:38:38 -0500

    attempted question

---
+++ AutoLayoutLab/AutoLayoutLab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AutoLayoutLab
+//
+//  Created by Maitree Bain on 11/7/19.
+//  Copyright © 2019 Maitree Bain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
